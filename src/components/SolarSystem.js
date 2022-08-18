6f25e5fb2bccf2ce5999152853fff2f27226fdb4
Pedro <pedrosteinmuller10105@hotmail.com>
2022-08-18 14:55:19 -0300

    resolucao requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        SolarSystem
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
